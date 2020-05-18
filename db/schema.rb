02857a7f753ce7bcd194968700c3c65bcc51a2bf
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-05-18 09:28:49 -0400

    Postgres database config

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_12_165422) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fingerprints", force: :cascade do |t|
     t.string "width"
     t.string "height"
